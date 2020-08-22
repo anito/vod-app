5b352559ed3ef110105bbc7c422609668df4e634
anito <axelnitzschner@mac.com>
2020-08-23 00:08:28 +0200

    updated 21

---
+++ node_modules/@sveltejs/site-kit/components/Modal.js
@@ -0,0 +1,8 @@
+import {classAdderBuilder} from '@sveltejs/site-kit/common/classAdderBuilder.js';
+import Div from '@sveltejs/site-kit/common/Div.svelte';
+
+export default classAdderBuilder({
+  class: 'mdc-drawer-scrim',
+  component: Div,
+  contexts: {}
+});
