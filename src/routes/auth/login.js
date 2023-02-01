20316ef488c1c254413ad1f284f48c83e4df7770
anito <axelnitzschner@mac.com>
2023-02-01 10:07:35 +0100

    changes

--- src/routes/auth/login.js
+++ src/routes/auth/login.js
@@ -4,7 +4,6 @@ export function post(req, res) {
   const { email, password } = { ...req.body };
   const { expires } = { ...req.session.cookie };
 
-  console.log({ ...req.session });
   api.post(`users/login${req.search}`, { email, password }).then((response) => {
 
     if (response.success) {
